cf3ece8d17e241c2b4ea5447837049d868bbb460
hornie <hornminer@gmail.com>
2018-08-31 08:38:57 +0300

    first commit

---
+++ website/config.js
@@ -0,0 +1,25 @@
+var api = "http://pool.hleb.cc:8117";
+
+var api_blockexplorer = "http://explorer.hleb.cc:18622";
+
+var poolHost = "pool.hleb.cc";
+
+var irc = "";
+
+var email = "pool@hleb.cc";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://explorer.hleb.cc/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://explorer.hleb.cc/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = {
+    "HLEB": [
+        ["pool.hleb.cc", "http://pool.hleb.cc:8117"],
+    ]
+};
